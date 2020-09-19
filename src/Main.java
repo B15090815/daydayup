f626e96fae2a32a5995be4062d22b4216f5a7af9
ErCHen <940909850@qq.com>
2020-09-19 10:52:46 +0800

    404 Sum of Left Leaves

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+
 import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
 
 public class Main {
 
@@ -20,6 +28,7 @@ public class Main {
             visited[i] = false;
         }
     }
+
     public List<List<Integer>> permuteUnique(int[] nums) {
         boolean[] visited = new boolean[nums.length];
         int[] permute = new int[nums.length];
@@ -36,6 +45,29 @@ public class Main {
         }
         return result;
     }
+
+     // https://leetcode-cn.com/problems/sum-of-left-leaves/
+    void dfs(TreeNode root, boolean isLeft, int[] ans) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null && isLeft) {
+            ans[0] += root.val;
+            return;
+        }
+
+        if (root.left !=null)
+            dfs(root.left, true, ans);
+
+        if (root.right != null)
+            dfs(root.right, false, ans);
+    }
+
+     public int sumOfLeftLeaves(TreeNode root) {
+         int[] ans = {0};
+         dfs(root, true, ans);
+         return ans[0];
+     }
+
 
 
     public static void main(String[] args) {
