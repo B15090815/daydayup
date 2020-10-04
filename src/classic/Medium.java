3d5290d07f0e33def17a3a3275cc8085c3960d97
ErCHen <940909850@qq.com>
2020-10-04 17:00:21 +0800

    find the median from the streaming data.

--- src/classic/Medium.java
+++ src/classic/Medium.java
@@ -1,5 +1,9 @@
 package classic;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Medium {
 //    https://leetcode-cn.com/problems/one-away-lcci/
     public boolean oneEditAway(String first, String second) {
@@ -49,7 +53,72 @@ public class Medium {
     }
 
     public static void main(String[] args) {
-        Medium solution = new Medium();
+//        Medium solution = new Medium();
+        MedianFinder a = new MedianFinder();
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            int n = s.nextInt();
+            a.addNum(n);
+            System.out.println(a.findMedian());
+        }
+//        int[] nums = {1,2,3,4,5,6,7,8};
+//        for (int num:nums) {
+//            a.addNum(num);
+//        }
 
+    }
+}
+
+// https://leetcode-cn.com/problems/continuous-median-lcci/
+class MedianFinder {
+    static Comparator<Integer> maxQcmp = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    };
+
+    int count;
+    PriorityQueue<Integer> minQue;
+    PriorityQueue<Integer> maxQue;
+
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        count = 0;
+        minQue = new PriorityQueue<>();
+        maxQue = new PriorityQueue<>(maxQcmp);
+    }
+
+    void check() {
+        if (!minQue.isEmpty() && !maxQue.isEmpty()) {
+            if (minQue.peek() < maxQue.peek()) {
+                int a = minQue.poll();
+                int b = maxQue.poll();
+                minQue.add(b);
+                maxQue.add(a);
+            }
+        }
+    }
+    public void addNum(int num) {
+        if ((count & 1) == 0) {
+            // even
+            maxQue.add(num);
+        } else {
+            // odd
+            minQue.add(num);
+        }
+        check();
+        count++;
+    }
+
+    public double findMedian() {
+        double ans = 0.0;
+        if ((count & 1) == 0) {
+            ans = (minQue.peek() + maxQue.peek()) / 2.0;
+        } else {
+            ans = (double) maxQue.peek();
+        }
+        return ans;
     }
 }
