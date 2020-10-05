5f8c7acf0a095496a160d51b200788615629d5de
ErCHen <940909850@qq.com>
2020-10-05 14:16:54 +0800

    solve leetcode 18

--- src/days/Medium.java
+++ src/days/Medium.java
@@ -1,6 +1,10 @@
 package days;
 import common.ListNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Medium {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode p = l1, q = l2;
@@ -39,7 +43,90 @@ public class Medium {
 
     }
 
-    public static void main(String[] args) {
+//    https://leetcode-cn.com/problems/4sum/
+    void dfs(int depth, int index, int[] temp, List<List<Integer>> ans, int[] nums, int target) {
+        if (index == 4) {
+            int sum = 0;
+            for (int a:temp
+            ) {
+                sum += a;
+            }
+            if (sum == target) {
+                List<Integer> copy = new ArrayList<>();
+                for (int a :
+                        temp) {
+                    copy.add(a);
+                }
+                ans.add(copy);
+            }
+            return;
+        }
 
+        if (depth == nums.length || index > 4) {
+            return;
+        }
+
+        for (int i = depth; i < nums.length; i++) {
+            if (i != depth && nums[i] == nums[i-1])
+                continue;
+            temp[index] = nums[i];
+            dfs(i+1, index+1, temp, ans, nums, target);
+        }
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] temp = new int[4];
+        dfs(0, 0, temp, ans, nums, target);
+        return ans;
+    }
+
+    public List<List<Integer>> fourSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        int L = nums.length;
+        for (int i = 0; i < L - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            for (int j = i + 1; j < L - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1])
+                    continue;
+
+                int k = j + 1, x = L - 1;
+                if (nums[i] + nums[j] + nums[k] + nums[k-1] > target)
+                    break;
+                if (nums[i] + nums[j] + nums[x-1] + nums[x] < target)
+                    continue;
+
+                while (k < x) {
+                    int sum = nums[i] + nums[j] + nums[k] + nums[x];
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[x]));
+                        while (k < x && nums[k] == nums[++k]);
+                        while (k < x && nums[x] == nums[--x]);
+                    } else if(sum < target) {
+                        while (k < x && nums[k] == nums[++k]);
+                    } else {
+                        while (k < x && nums[x] == nums[--x]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Medium solution = new Medium();
+//        int[] nums = {-499,-486,-479,-462,-456,-430,-415,-413,-399,-381,-353,-349,-342,-337,-336,-331,-330,-322,-315,-280,-271,-265,-249,-231,-226,-219,-216,-208,-206,-204,-188,-159,-144,-139,-123,-115,-99,-89,-80,-74,-61,-22,-22,-8,-5,4,43,65,82,86,95,101,103,123,149,152,162,165,168,183,204,209,209,220,235,243,243,244,248,253,260,273,281,284,288,290,346,378,382,384,407,411,423,432,433,445,470,476,497};
+        int[] nums = {-2,-1,-1,1,1,2,2};
+        int target = 0;
+        List<List<Integer>> ans = solution.fourSum2(nums, target);
+        for (List<Integer> temp: ans) {
+            for (Integer a: temp) {
+                System.out.print(a + "  ");
+            }
+            System.out.println();
+        }
     }
 }
