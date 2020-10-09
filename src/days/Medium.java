89aedf19173b96d305274b859ecd7ae778356e97
ErCHen <940909850@qq.com>
2020-10-09 22:46:48 +0800

    LCS

--- src/days/Medium.java
+++ src/days/Medium.java
@@ -188,4 +188,34 @@ public class Medium {
             i++;
         }
     }
+
+//    https://leetcode-cn.com/problems/first-common-ancestor-lcci/
+
+    TreeNode LCS(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+
+        if (root.val == p.val || root.val == q.val)
+            return root;
+
+        TreeNode left = LCS(root.left, p, q);
+        TreeNode right = LCS(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        }
+
+        if (right == null) {
+            return left;
+        }
+
+        if (left != right)
+            return root;
+
+        return left;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return LCS(root, p, q);
+    }
 }
